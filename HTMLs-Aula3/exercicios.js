8d002f61a303ae2babdd8ce9a1e191cfe7825f1d
Evellyn Ribeiro Sene Sim�es <54590135850@>
2025-03-27 11:30:50 -0300

    Aula 3 até exercicio 10

--- HTMLs-Aula3/exercicios.js
+++ HTMLs-Aula3/exercicios.js
@@ -48,13 +48,11 @@ const VerificacaoCNH = () => {
 
 //exercicio5-aula3
 const OrdemCrescente = () =>{
-    const n1 = document.getElementById("n1").value;
-    const n2 = document.getElementById("n2").value;
-    const n3 = document.getElementById("n3").value;
+    const n1 = Number(document.getElementById("n1").value);
+    const n2 = Number(document.getElementById("n2").value);
+    const n3 = Number(document.getElementById("n3").value);
 
-    const maior_numero = 0;
-    const medio_numero = 0;
-    const menor_numero = 0;
+    let maior_numero, menor_numero, medio_numero;
 
     if (n1 > n2 && n1 > n3){
         maior_numero = n1
@@ -75,7 +73,7 @@ const OrdemCrescente = () =>{
             medio_numero = n3
         }
     }else{
-        menor_numero = n3
+        maior_numero = n3
         if(n1> n2){
             medio_numero = n1
             menor_numero = n2
@@ -92,10 +90,29 @@ const OrdemCrescente = () =>{
 const ConversaoH_Min = () =>{
     const hora = Number(document.getElementById("hora").value);
     const calculo = hora * 60
-    document.getElementById("resultado").innerText = calculo
+    document.getElementById("resultado").innerText = `Essa hora são ${calculo} minutos.`
 }
 
 //exercicio7-aula3
+const Notas = () =>{
+    let valor = Number(document.getElementById("valor").value);
+    
+    const notas = [200, 100, 50, 20, 10, 5, 1];
+    let resultado ='';
+
+    for (let i =0; i< notas.length; i++){
+        let cedula = notas [i];
+
+        let quantidade = Math.floor(valor/cedula);
+        
+        if (quantidade > 0) {
+            resultado += `${quantidade} cédulas de R$${cedula}<br>`;
+        }
+        
+        valor %= cedula;
+    }
+    document.getElementById("resultado").innerHTML = resultado
+}
 
 //exercicio8-aula3
 const Raiz = () =>{
@@ -103,3 +120,42 @@ const Raiz = () =>{
     const raiz = Math.sqrt(numero)
     document.getElementById("resultado").innerText = raiz
 }
+
+//exercicio9-aula3
+const Forms = () =>{
+    const nome = document.getElementById("nome").value;
+    const endereco = document.getElementById("endereco").value;
+    const sexo = document.querySelector('input[name="sexo"]:checked')?.value;
+    const telefone = document.getElementById("telefone").value;
+    const celular = document.getElementById("celular").value;
+    const email = document.getElementById("email").value;
+
+    if (!nome || !endereco || !sexo || !telefone || !celular || !email){
+        document.getElementById("resultado").innerText = "Preencha todos os campos";
+        return;
+    }
+
+    document.getElementById("resultado").innerText = "Cadastrado com sucesso"
+}
+
+//exercicio10-aula3
+const Imposto = () =>{
+    let faixa= Number(document.getElementById("faixa").value);
+    let imposto = 0;
+
+    if (faixa <= 2112){
+        imposto = 0;
+    }else if(faixa <=3282){
+        imposto = (faixa - 2112) * 0.075 - 158.40;
+    }else if(faixa <= 4456){
+        imposto = (faixa - 3282) * 0.15 - 370.80 + 158.40;
+    }else if(faixa <=5634){
+        imposto = (faixa - 4456) * 0.225 - 602 + 370.80 + 158.40;
+    }else{
+        imposto = (faixa - 5634) * 0.275 - 826 + 602 + 370.80 + 158.40;
+    }
+
+    document.getElementById("resultado").innerText = `O imposto é ${imposto}`
+}
+    
+
